81f1bb7dbd9a9bf052378cd4467cb3a7434a96b9
numberdaar <171ankit.2020@gmail.com>
2024-08-17 12:52:42 +0530

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['fakestoreapi.com'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
