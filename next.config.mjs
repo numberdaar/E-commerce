dc8b276856a202a1866f23f80bfa33ed07a917ae
numberdaar <171ankit.2020@gmail.com>
2024-08-17 10:55:48 +0530

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['fakestoreapi.com'], // Add the domain here
+    },
+  };
+  
+  export default nextConfig;
+  
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,8 +1,10 @@
 // src/components/ProductCard.js
+import Image from 'next/image';
 const ProductCard = ({ product, onAddToCart }) => {
   return (
     <div className="border p-4 flex flex-col items-center">
-      <img src={product.image} alt={product.title} className="w-full h-48 object-cover mb-4" />
+      <Image src={product.image} alt={product.title} className="w-full h-48 object-cover mb-4" width={500}   // Set your desired width
+      height={500} />
       <h2 className="text-xl font-semibold">{product.title}</h2>
       <p className="text-gray-600">${product.price}</p>
       <button
